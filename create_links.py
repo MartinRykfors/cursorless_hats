9498505c99dcd47ac6021c0b61bea93f9524e709
Martin Rykfors <martinrykfors@evolabel.com>
2023-09-27 11:58:31 +0200

    Formalize path handling

--- create_links.py
+++ create_links.py
@@ -3,25 +3,29 @@ import os
 import shutil
 
 mapping = {
-    "bolt": "gate",
-    "crosshairs": "fang",
-    "curve": "knight",
-    "default": "default",
-    "ex": "rook",
-    "eye": "church",
-    "fox": "shroom",
-    "frame": "horn",
-    "hole": "wrench",
-    "play": "play",
-    "wing": "gem",
+    "bolt": "source/gate.svg",
+    "crosshairs": "source/fang.svg",
+    "curve": "source/knight.svg",
+    "default": "source/default.svg",
+    "ex": "source/rook.svg",
+    "eye": "source/church.svg",
+    "fox": "source/shroom.svg",
+    "frame": "source/horn.svg",
+    "hole": "source/wrench.svg",
+    "play": "source/play.svg",
+    "wing": "source/gem.svg",
 }
 
 if __name__ == "__main__":
-    if os.path.exists("hats"):
-        shutil.rmtree("hats")
-    os.makedirs("hats")
+    root = os.path.dirname(os.path.abspath(__file__))
+    output_dir = os.path.join(root, "hats")
+
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir)
+
     for spoken, source in mapping.items():
-        os.symlink(
-            os.path.join("..", "source", "{}.svg".format(source)),
-            os.path.join(".", "hats", "{}.svg".format(spoken)),
-        )
+        src = os.path.relpath(os.path.join(root, source), output_dir)
+        dst = os.path.join(output_dir, "{}.svg".format(spoken))
+        print("Linking {} -> {}".format(dst, src))
+        os.symlink(src, dst)
